ecf2ec686d4d81d5fb4fc91d6ed566a8c94ffc36
MostafaMaher919 <mostafamaher919@gmail.com>
2025-01-27 19:41:38 +0200

    opp

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -2,9 +2,52 @@
 {
     internal class Program
     {
+        public static void OptimizedBubbleSort(int[] arr)
+        {
+            int length = arr.Length;
+            bool swapped;
+            for (int i = 0; i < length - 1; i++)   // 10, 4, -5, -100, 4, 60 
+            {                                      // 4, 10, -5, -100, 4, 60
+                swapped = false;
+                for (int j = 0; j < length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        //swap
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                // no elements were swapped , the array is already sorted
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+        }
+        public static void PrintArray(int[] arr)
+        {
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int[] arr = { 10, 4, -5, -100, 4, 60 };
+            //Console.WriteLine("original array = :");
+            //PrintArray(arr);
+
+            //OptimizedBubbleSort(arr);
+            //PrintArray(arr);
+
+            Range<int> intRange = new Range<int>(5, 10);
+            Console.WriteLine("is in range" + intRange.IsInRange(15));
+            Console.WriteLine("length of range = " + intRange.Length());
+
         }
     }
 }
---
+++ ConsoleApp6/Range1.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp6
+{
+    public class Range<T> where T : IComparable<T>
+    {
+        public T _Min { get; set; }
+        public T _Max { get; set; }
+        public Range(T min, T max)
+        {
+            _Min = min;
+            _Max = max;
+        }
+
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(_Min) >= 0 && value.CompareTo(_Max) <= 0;
+        }
+        public T Length()
+        {
+            dynamic min = _Min;
+            dynamic max = _Max;
+            return max - min;
+        }
+
+    }
+}
